1941b6f9e578e1d67fe4cdb990675ba2b666a9cf
icarus496 <bendobozy@gmail.com>
2018-09-29 17:02:13 -0400

    tic tac toe almost working

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-class Square:
-    def __init__(self, position, state):
-        self.position = position # A tuple containing x and y coordiantes of the square, (0,0) being the upper left corner, and (2,2) being the lower right
-        self.state = state # Either "x", "o" or 0.
-    def setState(self, newState):
-        self.state = newState
-class Bot:
+
+class Game:
+    def __init__(self):
+        self.turnNum = 0
+        self.winner = None
+        self.board = [[" "," "," "],[" ", " ", " "],[" ", " ", " "]]
+
+    def drawBoard(self):  # draws the board one row at a time
+        for row in range(len(self.board)):
+            if row == 0:  # special case, allows me to print the column numbers
+                print "  " + "1 " + "2 " + "3 "
+            print str(row+1),  # print the row numbers
+            for square in range(3):
+                print str(self.board[row][square]),  # print the item in that row
+            print ""
+
+    def setSquare(self, row, column, value):
+        self.board[row][column] = value
+
+    def turn(self):
+        self.drawBoard()
+        if self.turnNum % 2 == 0:
+            currentTurnLetter = "x"
+        else:
+            currentTurnLetter = "o"
+        print "It is " + currentTurnLetter + "'s turn"
+        while True:
+            while True:
+                try:
+                    userSelectionX = int(raw_input("Select Column. >> "))
+                    userSelectionY = int(raw_input("Select Row. >>"))
+                    break
+                except ValueError:
+                    print "Enter a valid number"
+            if (userSelectionX > 3) or (userSelectionY > 3):
+                print "Enter a valid number"
+            else:
+                break
+        self.setSquare(userSelectionY-1, userSelectionX-1, currentTurnLetter)
+        self.turnNum += 1
+        self.drawBoard()
+
+
+
+"""class Bot:
     def __init__(self, team, game):
         self.team = team
         self.moves = {} #this dictionary will hold the optimal move in each scenario
@@ -12,20 +50,7 @@ class Bot:
     def move(self):
         try:
             movesList = self.moves[self.game.getGameState()]
-            highestValue = 0;
-            for move in movesList:
+            highestValue = 0;"""
 
-class Board:
-    def __init__(self, squareList):
-        self.containedSquares = squareList # Defining the squares on the board
-    def reset(self):
-        for square in self.containedSquares:
-            square.setState(0)
-class Game:
-    turn = 0;
-    def __init__(self, board):
-        self.gameBoard = board
-    def getGameState(self):
-        return self.board.containedSquares
-
+test = Game()  # for testing purposes
 
